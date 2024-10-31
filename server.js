806de28b0832b1595a3d7f537c39348b7f656702
vickicoeur <victoriaoruba06@gmail.com>
2024-10-31 09:54:51 +0100

    Ajout des attributs aux jetons

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const publicVapidKey =
   "BD5mKbhDQU8y5KmsqwOYQnIs3yhOY7VwdNEswhMY872Xf4z8HbgWOMAKXPeXSmnTHSWOWJvSxhfxj6CMw9fQAZ0";
 const privateVapidKey = "MjvvNM4Mz0D1pUIWyRS1rAgiJAQLpLpn7cxlwKdI5G0";
 
+// Remplacez `audience` par une adresse `mailto:`
 webpush.setVapidDetails(
   "mailto:exemple@votre-email.com",
   publicVapidKey,
@@ -25,14 +26,24 @@ app.use(bodyParser.json());
 app.post("/subscribe", (req, res) => {
   const subscription = req.body;
 
+  const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // 24 heures
+  subscription.expirationTime = expirationDate;
+
+  // Afficher l'objet de souscription reçu
+
   // Envoie un statut 201 pour indiquer que l'abonnement a été reçu
   res.status(201).json({});
 
   // Message de notification
-  const payload = JSON.stringify({ title: "Nouvelle Notification!" });
+  const payload = JSON.stringify({ title: "Nouvelle Notification !" });
+
+  // Définir la TTL pour une durée de 24 heures
+  const options = {
+    TTL: 24 * 60 * 60, // Durée en secondes (24 heures)
+  };
 
   // Envoie de la notification push
-  webpush.sendNotification(subscription, payload).catch((error) => {
+  webpush.sendNotification(subscription, payload, options).catch((error) => {
     console.error("Erreur lors de l'envoi de la notification:", error);
   });
 });
