2cbb2fe05bd79a233ce967f75504ec307daec689
Zankha <hej.vrignaud@gmail.com>
2024-10-31 12:10:51 +0100

    sauvegarde des abonnements coté serveur, puis envoi d'une notification aux abonnements enregistrés (test)

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const publicVapidKey =
   "BD5mKbhDQU8y5KmsqwOYQnIs3yhOY7VwdNEswhMY872Xf4z8HbgWOMAKXPeXSmnTHSWOWJvSxhfxj6CMw9fQAZ0";
 const privateVapidKey = "MjvvNM4Mz0D1pUIWyRS1rAgiJAQLpLpn7cxlwKdI5G0";
 
+const subscriptions = [];
+
 // Remplacez `audience` par une adresse `mailto:`
 webpush.setVapidDetails(
   "mailto:exemple@votre-email.com",
@@ -28,6 +30,23 @@ app.post("/subscribe", (req, res) => {
 
   const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // 24 heures
   subscription.expirationTime = expirationDate;
+
+  const authTokens = [];
+  subscriptions.forEach(element => {
+    authTokens.push(element.keys.auth)
+  });
+
+  if(!(authTokens.includes(subscription.keys.auth))){
+    console.log("Souscription non enregistrée, enregistrement")
+    subscriptions.push(subscription);
+  }
+
+  if(authTokens.includes(subscription.keys.auth)){
+    console.log("Souscription deja enregistrée");
+  }
+
+  // console.log(subscriptions);
+  //enregistrement coté serveur de la subscription
 
   // Afficher l'objet de souscription reçu
 
@@ -50,8 +69,26 @@ app.post("/subscribe", (req, res) => {
   webpush.sendNotification(subscription, payload, options).catch((error) => {
     console.error("Erreur lors de l'envoi de la notification:", error);
   });
+
+
 });
 
 app.listen(port, () =>
   console.log(`Server running on http://localhost:${port}`)
 );
+
+
+async function sendNewNotification(payload){
+  await Promise.all(subscriptions.map(async (sub) => {
+    try {
+      await webpush.sendNotification(sub, payload); // throws if not successful
+      console.log("Notif envoyée")
+    } catch (err) {
+      console.log(sub.endpoint, '->', err.message);
+      // TODO: Delete subscription (e.g. from db)
+    }
+  }));
+}
+
+setTimeout(()=> {
+  sendNewNotification('Test Notification')}, 10000)
